cd3607df8375b0af2a0396739fd26de1dcd3ffdc
LeLiver <2020211023007@cuc.edu.cn>
2024-01-15 19:40:28 +0800

    Event
    Make EventManager better

--- src/Unity/Assets/Events/Scripts/EventManager.cs
+++ src/Unity/Assets/Events/Scripts/EventManager.cs
@@ -18,25 +18,22 @@ public enum Operators
 // 选择结构体定义
 public struct Option
 {
-    public string element; //效果名称
-    public float value;  // 效果数值
+    public string name; // 选项名称
+    public string element; // buff效果名称
+    public float value;  // buff效果数值
     public string description; // 效果描述
     public float durationTime; // 效果持续时间
 }
 
-// 触发条件结构体定义
-public struct Trigger
-{
-    string triggerElement;
-    float triggerValue;
-}
 // 事件结构体定义
 public struct Event
 {
-    public string text;                  // 事件文本
-    public Option[] eventOptions;  // 包含三个选择的数组
-
-    public Trigger[] eventTriggers; // 包含该事件若干触发条件
+    public string title; // 事件标题
+    public string content; // 事件文本
+    public Image image; // 事件插图
+    public int optionNumbers; // 包含选择数
+    public Option[] eventOptions; // 包含若干选择的数组
+    public Dictionary<string, float> eventTriggers; // 包含该事件若干触发条件的数组
 }
 
 // 事件链结构体定义
@@ -44,15 +41,26 @@ public struct EventChain
 {
     public int eventCounts;
     public Event[] events;  // 包含多个事件的数组
+    public Event* pt_event;
 }
 
 public class EventManager
 {
-    private EventChain _eventChain;
+
+    private EventChain[] _eventChain;
+    public GameObject eventPanel;
     // Start is called before the first frame update
     void Start()
     {
+        eventPanel = gameObject.Find("EventPanel");
         
+        eventPanel.title = gameObject.transform.Find("Title").GetComponent<Text>();
+        eventPanel.contents = gameObject.transform.Find("Contents").GetComponent<Text>();
+        eventPanel.image = gameObject.transform.Find("Image").GetComponent<Image>();
+        eventPanel.choice_buttons = new Button[3];
+        eventPanel.choice_buttons[0] = gameObject.transform.Find("Choice1").GetComponent<Button>();
+        eventPanel.choice_buttons[1] = gameObject.transform.Find("Choice2").GetComponent<Button>();
+        eventPanel.choice_buttons[2] = gameObject.transform.Find("Choice3").GetComponent<Button>();
     }
 
     // Update is called once per frame
@@ -76,6 +84,74 @@ public class EventManager
         return eventChain;
     }
 
+    // 设置时间窗口并显示时间窗口
+    public void SetEventPanel(Event currentEvent)
+    {
+        eventPanel.title.text = currentEvent.title;
+        eventPanel.contents.text = currentEvent.content;
+        eventPanel.image = currentEvent.image;
+        for (int i = 0; i < currentEvent.optionNumbers; i++)
+        {
+            eventPanel.choice_buttons[i].GetComponentInChildren<Text>().text = currentEvent.eventOptions[i].name;
+        }
+
+        eventPanel.SetActive(true);
+    }
+    // 判定触发条件
+    public bool IsTrigger(Event currentEvent)
+    {
+        bool isTrigger = false;
+
+        if(currentEvent.eventTriggers.Count > 0)
+        {
+            foreach (KeyValuePair<string, float> kvp in currentEvent.eventTriggers)
+            {
+                if(_data[currentEvent.eventTriggers.key]>=currentEvent.eventTriggers.value)
+                {
+                    isTrigger = true;
+                }
+                else{
+                    isTrigger = false;
+                }
+            }
+        }
+        return isTrigger;
+    }
+    // 选择按钮1
+    public bool ChoiceButtonFirst(Event currentEvent, int i)
+    {
+        return 1;
+    }
+    // 选择按钮2
+    public bool (Event currentEvent, int i)
+    {
+        return 2;
+    }
+    // 选择按钮3
+    public bool (Event currentEvent, int i)
+    {
+        return 3;
+    }
+    // 选择
+    public void SelectOption(Event currentEvent)
+    {
+        int optionIndex;
+        Option currentOption = currentEvent.eventOptions[optionIndex];
+    }
+    // 遍历待选事件
+    public void Circuit()
+    {
+        foreach(EventChain currentChain in _eventChain)
+        {
+            bool isTrigger = IsTrigger(pt_event);
+
+            if(isTrigger)
+            {
+                SetEventPanel(Event pt_event);
+            }
+
+        }
+    }
     // buff导入函数
     public void importBuff(Event eventInstance, int playerChioceIndex)
     {
